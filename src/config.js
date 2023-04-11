4ca69a0e2a5d98205347d435d32e9eac29da536d
Shamimzz <shamimislam5091@gmail.com>
2023-04-11 13:59:37 +0600

    added

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
-let BASE_URL = "https://post-it-heroku.herokuapp.com/";
+let BASE_URL = "https://sociallyserverapi.onrender.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
 
 export { BASE_URL };
+
