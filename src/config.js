52cc990c306e41f740e2961f5d4535208673a74f
Shamimzz <shamimislam5091@gmail.com>
2023-04-11 14:17:42 +0600

    added

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 let BASE_URL = "https://sociallyserverapi.onrender.com/";
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
-}
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+//   BASE_URL = "http://localhost:4000/";
+// }
 
 export { BASE_URL };
 
